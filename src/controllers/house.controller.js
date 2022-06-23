47b1e23941393d570e31b09c46d02041a9814b53
Mayowa dabiri <mayowad43@gmail.com>
2022-06-23 12:16:55 +0100

    commit files

--- src/controllers/house.controller.js
+++ src/controllers/house.controller.js
@@ -1,9 +1,9 @@
-const House = require("../models/house.model");
-const cloudinaryUploadMethod = require("../utils/cloudinary");
-const path = require("path");
-const { successResMsg, errorResMsg } = require("../utils/appResponse");
-const AppError = require("../utils/appError");
-require("dotenv").config();
+const House = require('../models/house.model');
+const cloudinaryUploadMethod = require('../utils/cloudinary');
+const path = require('path');
+const { successResMsg, errorResMsg } = require('../utils/appResponse');
+const AppError = require('../utils/appError');
+require('dotenv').config();
 
 //creating data to post houses
 exports.addHouse = async (req, res, next) => {
@@ -24,33 +24,47 @@ exports.addHouse = async (req, res, next) => {
       photos,
     } = req.body;
 
-    if (
-      !address ||
-      !city ||
-      !state ||
-      !description ||
-      !isItFurnished ||
-      !propertyType ||
-      !bedroom ||
-      !bathroom ||
-      !toilet ||
-      !amenities ||
-      !price ||
-      !negotiable ||
-      !photos
-    ) {
-      return res.status(401).json({
-        message: "Please Fill in the required fields",
+    // if (
+    //   !address ||
+    //   !city ||
+    //   !state ||
+    //   !description ||
+    //   !isItFurnished ||
+    //   !propertyType ||
+    //   !bedroom ||
+    //   !bathroom ||
+    //   !toilet ||
+    //   !amenities ||
+    //   !price ||
+    //   !negotiable ||
+    //   !photos
+    // ) {
+    //   return res.status(401).json({
+    //     message: "Please Fill in the required fields",
+    //   });
+    // }
+    // console.log(req.body);
+    console.log(req.files);
+    newFIles = req.files.map((item, index) => {
+      req.body.photos.map((des, i) => {
+        if (index === i) {
+          item.description = des.description;
+        }
+        return item;
       });
-    }
+    });
+    console.log(req.files);
     let urls = [];
     let files = req.files;
-    if (!files) return next(new AppError("No picture attached..", 400));
+    if (!files) return next(new AppError('No picture attached..', 400));
     for (let file of files) {
       let { path } = file;
       let newPath = await cloudinaryUploadMethod(path);
 
-      urls.push(newPath);
+      urls.push({
+        url: newPath,
+        description: file.description,
+      });
     }
     photos = urls.map((url) => url.res);
     let newHouse = await House.create({
@@ -66,11 +80,11 @@ exports.addHouse = async (req, res, next) => {
       amenities,
       price,
       negotiable,
-      photos,
+      urls,
       //user: req.user.id,
     });
     return successResMsg(res, 201, {
-      message: "House added successfully",
+      message: 'House added successfully',
       newHouse,
     });
   } catch (error) {
@@ -87,7 +101,7 @@ exports.updateHouseInfo = async (req, res, next) => {
       new: true,
     });
     return successResMsg(res, 200, {
-      message: "success",
+      message: 'success',
       houseUpdated,
     });
   } catch (error) {
@@ -101,7 +115,7 @@ exports.searchForHouse = async (req, res, next) => {
     const { _id } = req.params;
     const searchHouse = await House.find({ _id });
     return successResMsg(res, 200, {
-      message: "success",
+      message: 'success',
       searchHouse,
     });
   } catch (error) {
@@ -115,7 +129,7 @@ exports.viewCategory = async (req, res, next) => {
     const { category } = req.params;
     const viewCategory = await House.find({ category });
     return successResMsg(res, 200, {
-      message: "success",
+      message: 'success',
       viewCategory,
     });
   } catch (error) {
@@ -128,7 +142,7 @@ exports.fetchHouse = async (req, res, next) => {
   try {
     const viewHouse = await House.find();
     return successResMsg(res, 200, {
-      message: "success",
+      message: 'success',
       viewHouse,
     });
   } catch (error) {
@@ -142,7 +156,7 @@ exports.viewBedroomType = async (req, res, next) => {
     const { bedroomType } = req.params;
     const bedroomTypes = await House.find({ bedroomType });
     return successResMsg(res, 200, {
-      message: "success",
+      message: 'success',
       bedroomTypes,
     });
   } catch (error) {
@@ -155,7 +169,7 @@ exports.addhouseimage = async (req, res, next) => {
   try {
     const urls = [];
     const files = req.files;
-    if (!files) return next(new AppError("No picture attached..", 400));
+    if (!files) return next(new AppError('No picture attached..', 400));
     for (const file of files) {
       const { path } = file;
       const newPath = await cloudinaryUploadMethod(path);
@@ -170,10 +184,9 @@ exports.addhouseimage = async (req, res, next) => {
 
 exports.fetchBookmarkList = async (req, res, next) => {
   try {
-    
     const bookmarkList = await House.find();
     return successResMsg(res, 200, {
-      message: "Bookmark listed successfully",
+      message: 'Bookmark listed successfully',
       bookmarkList,
     });
   } catch (error) {
@@ -183,10 +196,9 @@ exports.fetchBookmarkList = async (req, res, next) => {
 
 exports.fetchPropertyDetails = async (req, res, next) => {
   try {
-    
     const propertyDetails = await House.find();
     return successResMsg(res, 200, {
-      message: "Property displayed successfully",
+      message: 'Property displayed successfully',
       propertyDetails,
     });
   } catch (error) {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,26 +1,30 @@
-const express = require("express");
-const User = require("../controllers/user.controller");
+const express = require('express');
+const User = require('../controllers/user.controller');
 const router = express.Router();
-const { authorize } = require("../middleware/auth.middleware");
+const { authorize } = require('../middleware/auth.middleware');
 
 //users sign up path
-router.post("/registerUser", User.register);
+router.post('/registerUser', User.register);
 //users login path
-router.post("/loginUser", User.login);
+router.post('/loginUser', User.login);
 //fetch users path
-router.get("/viewAllUser", authorize, User.fetchAllUsers);
+router.get('/viewAllUser', authorize, User.fetchAllUsers);
 //users logout path
-router.get("/logoutUser", User.logout);
+router.get('/logoutUser', User.logout);
 
 //all bookmark services path
 
-router.post("/auth/user/verify", User.verifyEmail);
-router.post("/auth/user/resend-verification-mail", User.resendVerificationMail);
-router.post("/auth/user/password-reset-url", User.forgetPasswordLink);
-router.patch("/auth/user/update-password", User.updatePassword);
-router.patch("/auth/user/reset-password", User.resetPassword);
-router.delete("/deleteHouse/:_id", User.deleteHouse);
-router.patch("/auth/user/edit-profile/:_id", User.editUser);
-router.get("/auth/user/fetch-userDetails", User.fetchloggedUserDetails);
+router.post('/auth/user/verify', User.verifyEmail);
+// d-verification-mail", User.resendVerificationMail);
+router.post('/auth/user/password-reset-url', User.forgetPasswordLink);
+router.patch('/auth/user/update-password', User.updatePassword);
+router.patch('/auth/user/reset-password', User.resetPassword);
+router.delete('/deleteHouse/:_id', User.deleteHouse);
+router.patch('/auth/user/edit-profile/:_id', User.editUser);
+router.get(
+  '/auth/user/fetch-userDetails',
+  authorize,
+  User.fetchloggedUserDetails
+);
 
 module.exports = router;
